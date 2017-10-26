735bd6da647849eea994ae561493727ce74d321c
Alex Shnayder <alex@shnayder.pro>
2017-10-26 17:21:05 +0700

    Update handler tags on hooking based on the events configuration

--- src/Lifecycle.js
+++ src/Lifecycle.js
@@ -1,9 +1,73 @@
 const Hooter = require('hooter')
 const { next } = require('hooter/effects')
+const events = require('./events')
 const { optionsToObject, compareNames } = require('./common')
 
 
+function expandTags(tags, tagsConfig, result = []) {
+  let impliedTags = []
+
+  for (let i = 0; i < tags.length; i++) {
+    let tag = tags[i]
+    let implies = tagsConfig[tag] && tagsConfig[tag].implies
+
+    if (!result.includes(tag)) {
+      result.push(tag)
+    }
+
+    if (implies) {
+      impliedTags.push(implies)
+    }
+  }
+
+  if (impliedTags.length) {
+    expandTags(impliedTags, tagsConfig, result)
+  }
+
+  return result
+}
+
+// WARNING: mutates handler
+function updateHandlerTags(handler) {
+  let { event, tags, goesBefore, goesAfter } = handler
+  let eventTags = events[event] && events[event].tags
+
+  if (!tags || !tags.length || !eventTags) {
+    return handler
+  }
+
+  tags = expandTags(tags, eventTags)
+
+  for (let i = 0; i < tags.length; i++) {
+    let tag = tags[i]
+    let tagGoesBefore = eventTags[tag] && eventTags[tag].goesBefore
+    let tagGoesAfter = eventTags[tag] && eventTags[tag].goesAfter
+
+    if (tagGoesBefore) {
+      goesBefore = goesBefore || []
+      goesBefore.push(...tagGoesBefore)
+    }
+
+    if (tagGoesAfter) {
+      goesAfter = goesAfter || []
+      goesAfter.push(...tagGoesAfter)
+    }
+  }
+
+  handler.tags = tags
+  handler.goesBefore = goesBefore && goesBefore
+  handler.goesAfter = goesAfter && goesAfter
+
+  return handler
+}
+
+
 class LifecycleProxy extends Hooter.HooterProxy {
+  createHandler(...args) {
+    let handler = super.createHandler(...args)
+    return updateHandlerTags(handler)
+  }
+
   _createTapOrHandleHook(command, handler, checkIfCommandIsFinal) {
     if (typeof command !== 'string' && !Array.isArray(command)) {
       throw new Error('A command must either be a string or an array of strings')
