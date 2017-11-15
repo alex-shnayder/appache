25e32a1976cc14052eae8c5f77944a2eccb8073a
Alex Shnayder <alex@shnayder.pro>
2017-11-15 07:29:10 +0700

    Make command restriction work for more than one level of unknown commands

--- src/plugins/restrict/index.js
+++ src/plugins/restrict/index.js
@@ -4,13 +4,45 @@ const modifySchema = require('./modifySchema')
 const handleUnknownCommand = require('./handleUnknownCommand')
 
 
-function validateCommandOptions(options) {
+function findClosestParentWithConfig(config, fullName) {
+  let parentFullName = fullName.slice(0, -1)
+  let parentConfig = findCommandByFullName(config, parentFullName, true)
+
+  if (!parentConfig && parentFullName.length) {
+    return findClosestParentWithConfig(config, parentFullName)
+  }
+
+  return parentConfig
+}
+
+function validateOptions(options) {
   let option = options.find((option) => !option.config)
 
   if (option) {
     throw new InputError(`Unknown option "${option.inputName}"`)
   }
 }
+
+function validateCommand(config, command) {
+  let { fullName, options, config: commandConfig } = command
+
+  if (!commandConfig) {
+    let isValid = (fullName.length > 1)
+    let parentConfig
+
+    if (isValid) {
+      parentConfig = findClosestParentWithConfig(config, fullName)
+      isValid = parentConfig && !parentConfig.restrictCommands
+    }
+
+    if (!isValid) {
+      handleUnknownCommand(command, parentConfig)
+    }
+  } else if (commandConfig.restrictOptions && options) {
+    validateOptions(options)
+  }
+}
+
 
 module.exports = function* restrict() {
   yield preHook({
@@ -24,24 +56,5 @@ module.exports = function* restrict() {
   yield preHook({
     event: 'process',
     goesAfter: ['modifyCommand', 'modifyOption'],
-  }, (config, command) => {
-    let { fullName, options, config: commandConfig } = command
-
-    if (!commandConfig) {
-      let isValid = (fullName.length > 1)
-      let parentConfig
-
-      if (isValid) {
-        let parentFullName = fullName.slice(0, -1)
-        parentConfig = findCommandByFullName(config, parentFullName, true)
-        isValid = parentConfig && !parentConfig.restrictCommands
-      }
-
-      if (!isValid) {
-        handleUnknownCommand(command, parentConfig)
-      }
-    } else if (commandConfig.restrictOptions && options) {
-      validateCommandOptions(options)
-    }
-  })
+  }, validateCommand)
 }
