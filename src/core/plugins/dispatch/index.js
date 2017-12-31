737fa3c9b14fdd0339110975050dae19cbdbef46
Alex Shnayder <alex@shnayder.pro>
2017-12-31 15:47:18 +0700

    Assign all the properties of an execute event to all of its child dispatch events

--- src/core/plugins/dispatch/index.js
+++ src/core/plugins/dispatch/index.js
@@ -2,15 +2,24 @@ const { toot, hook, hookEnd } = require('hooter/effects')
 const { Result } = require('../../common')
 
 
+function createDispatchEvent(parentEvent, args) {
+  let event = Object.assign({}, this)
+  event.name = 'dispatch'
+  event.args = args
+
+  delete event.mode
+  delete event.tooter
+  delete event.cb
+
+  return event
+}
+
 function* dispatchCommands(config, batch) {
   let context
 
   for (let i = 0; i < batch.length; i++) {
-    context = yield yield toot({
-      name: 'dispatch',
-      args: [config, batch[i], context],
-      source: this.source,
-    })
+    let event = createDispatchEvent(this, [config, batch[i], context])
+    context = yield yield toot(event)
 
     if (context instanceof Result) {
       context.command = context.command || batch[i]
