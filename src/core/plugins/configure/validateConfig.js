cc6d793578600e0b5740da551895f316d34d5db7
Alex Shnayder <alex@shnayder.pro>
2018-01-08 23:44:30 +0700

    Avoid ajv caching to allow multiple appache instances

--- src/core/plugins/configure/validateConfig.js
+++ src/core/plugins/configure/validateConfig.js
@@ -42,6 +42,12 @@ function validateCommand(command) {
 }
 
 function validateConfig(schema, config) {
+  // ajv caches schemas by their ids effectively preventing us from using
+  // multiple instances of appache with a single instance of ajv, so we bust
+  // the cache on every config validation as a workaround.
+  // TODO: we should use an ajv instance per appache instance and only clear
+  // the cache when the schema changes
+  ajv.removeSchema()
   let isValid = ajv.validate(schema, config)
 
   if (!isValid) {
